1b94133303abb4ec52ceded9caf07cecc4d1cdd0
Prg-maker <df3296709@gmail.com>
2022-07-08 09:39:46 -0300

    add routes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
 
 import AppLoading from 'expo-app-loading';
 import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
 import { Home } from './src/screens/Home';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,7 +29,12 @@ export default function App() {
   
   return (
     <View>
-      <Home/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+          <Home/>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/Home/Registration/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export function Registration(){
+  return(
+    <View style={styles.container}>
+      <Text>daniel</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    height: '100%',
+    justifyContent:'center',
+    alignItems:"center"
+  }
+})
